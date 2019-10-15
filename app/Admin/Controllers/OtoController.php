1e5ed4f171fbd735e52b36e9c4df98301476a26e
nguyenthuy <thuy@relotalent.com>
2019-10-15 18:04:27 +0700

    oto admin

---
+++ app/Admin/Controllers/OtoController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Admin\Controllers;
+
+use App\Http\Controllers\HelperController;
+use App\Models\Oto;
+use Encore\Admin\Controllers\AdminController;
+use Encore\Admin\Form;
+use Encore\Admin\Grid;
+use Encore\Admin\Show;
+
+class OtoController extends AdminController
+{
+    /**
+     * Title for current resource.
+     *
+     * @var string
+     */
+    protected $title = 'App\Oto';
+
+    /**
+     * Make a grid builder.
+     *
+     * @return Grid
+     */
+    protected function grid()
+    {
+        $grid = new Grid(new Oto);
+        $grid->id('ID')->sortable();
+        $grid->column('ten');
+        $grid->column('avatar')->display(function($avatar){
+            return "<img src=\"".HelperController::getImageUrl($avatar)."\"/>";
+        });
+        $grid->gia()->display(function($gia){
+            return $gia . ' trieu VND';
+        });
+
+
+        return $grid;
+    }
+
+    /**
+     * Make a show builder.
+     *
+     * @param mixed $id
+     * @return Show
+     */
+    protected function detail($id)
+    {
+        $show = new Show(Oto::findOrFail($id));
+
+
+
+        return $show;
+    }
+
+    /**
+     * Make a form builder.
+     *
+     * @return Form
+     */
+    protected function form()
+    {
+        $form = new Form(new Oto);
+
+
+
+        return $form;
+    }
+}
