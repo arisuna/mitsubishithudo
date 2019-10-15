92d958cef3872f84c6e73f33d2d10f9cde4259bc
nguyenthuy <thuy@relotalent.com>
2019-10-15 16:10:52 +0700

    Initial commit

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,12 @@
+<footer class="text-muted">
+    <div class="container">
+        <p class="float-right">
+            <a href="#">Back to top</a>
+        </p>
+        <p>Album example is &copy; Bootstrap, but please download and customize it for yourself!</p>
+        <p>New to Bootstrap? <a href="../../">Visit the homepage</a> or read our <a href="../../getting-started/">getting started guide</a>.</p>
+    </div>
+</footer>
+<script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" ></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js"></script>
---
+++ resources/views/pages/login.blade.php
@@ -0,0 +1,8 @@
+<div class="row">
+    <div class="col-md-6">
+        Test
+    </div>
+    <div class="col-md-6">
+        Test right
+    </div>
+</div>
